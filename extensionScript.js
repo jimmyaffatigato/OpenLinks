422e2b565f181d9d833eac05d8e25c02c30df2b1
jimmyaffatigato <jimmyaffatigato@gmail.com>
2020-06-25 13:18:37 -0400

    Basically working

--- extensionScript.js
+++ extensionScript.js
@@ -1,1 +1,25 @@
-browser.tabs.executeScript({file: "test.js"})
+function listenForClicks() {
+    document.addEventListener("click", (e) => {
+        const pattern = document.getElementById("patternInput").value;
+        function sendMsg(tabs) {
+            browser.tabs.sendMessage(tabs[0].id, {
+                command: "open",
+                pattern: pattern,
+            });
+        }
+        function reportError(error) {
+            console.error(`${error}`);
+        }
+        browser.tabs
+            .query({ active: true, currentWindow: true })
+            .then(sendMsg)
+            .catch(reportError);
+    });
+}
+function reportExecuteScriptError(error) {
+    console.error(`Failed to execute content script: ${error.message}`);
+}
+browser.tabs
+    .executeScript({ file: "openLinks.js" })
+    .then(listenForClicks)
+    .catch(reportExecuteScriptError);
