4c74693283d6e8b4fc5ea003c3509eacba4f457a
jimmyaffatigato <jimmyaffatigato@gmail.com>
2020-06-26 00:21:15 -0400

    Compiled scripts from TS source

--- scripts/extensionScript.js
+++ scripts/extensionScript.js
@@ -2,54 +2,47 @@ const goButton = document.getElementById("goButton");
 const patternInput = document.getElementById("patternInput");
 const useRegex = document.getElementById("useRegex");
 const caseSensitive = document.getElementById("caseSensitive");
-
 let links = [];
-
 const openLinks = (links) => {
     links.forEach((link) => {
         browser.tabs.create({ url: link, active: false });
     });
 };
-
+const pollLinks = () => {
+    const pattern = patternInput.value;
+    function sendMsg(tabs) {
+        browser.tabs.sendMessage(tabs[0].id, {
+            command: "getLinks",
+            pattern: pattern,
+            useRegex: useRegex.checked,
+            caseSensitive: caseSensitive.checked,
+        });
+    }
+    browser.tabs.query({ active: true, currentWindow: true }).then(sendMsg);
+};
 const listenForClicks = () => {
-    const reportError = (error) => {
-        console.error(`${error}`);
-    };
     goButton.addEventListener("click", () => {
         openLinks(links);
     });
-    const pollLinks = () => {
-        function sendMsg(tabs) {
-            browser.tabs.sendMessage(tabs[0].id, {
-                command: "getLinks",
-                pattern: patternInput.value,
-                useRegex: useRegex.checked,
-                caseSensitive: caseSensitive.checked,
-            });
-        }
-        browser.tabs
-            .query({ active: true, currentWindow: true })
-            .then(sendMsg)
-            .catch(reportError);
-    };
     patternInput.addEventListener("input", pollLinks);
     useRegex.addEventListener("change", pollLinks);
     caseSensitive.addEventListener("change", pollLinks);
 };
-browser.tabs
-    .executeScript({ file: "scripts/openLinks.js" })
-    .then(listenForClicks);
-
+browser.tabs.executeScript({ file: "scripts/openLinks.js" }).then(listenForClicks);
 browser.runtime.onMessage.addListener((message) => {
     if (message.command === "links") {
         links = message.links;
-        document.getElementById("linkCount").innerHTML = `Found ${
-            message.links.length
-        } link${message.links.length !== 1 ? "s" : ""}.`;
+        document.getElementById("linkCount").innerHTML = `Found ${message.links.length} link${message.links.length !== 1 ? "s" : ""}.`;
         document.getElementById("links").innerHTML = message.links
             .map((link) => {
-                return `<a class="link" href="${link}">${link}</a>`;
-            })
+            return `<a class="link" href="${link}">${link}</a>`;
+        })
             .join("\n");
+    }
+    if (message.command === "filter") {
+        patternInput.value = message.pattern;
+        useRegex.checked = message.useRegex;
+        caseSensitive.checked = message.caseSensitive;
+        pollLinks();
     }
 });
--- scripts/openLinks.js
+++ scripts/openLinks.js
@@ -1,36 +1,48 @@
 (() => {
     const getLinks = (pattern, caseSensitive) => {
-        const as = document.getElementsByTagName("a");
+        const linkElements = document.getElementsByTagName("a");
         let links = [];
-        for (let a of as) {
-            links.push(a.href);
+        for (let i = 0; i < linkElements.length; i++) {
+            links.push(linkElements[i].href);
         }
         const set = new Set(links);
-        return [...set].filter((link) => {
+        const matchingLinks = [...set].filter((link) => {
             if (typeof pattern == "string") {
                 if (caseSensitive) {
                     return link.includes(pattern);
-                } else {
+                }
+                else {
                     return link.toLowerCase().includes(pattern.toLowerCase());
                 }
             }
             return link.match(pattern);
         });
+        return matchingLinks;
     };
-
+    const sendLinks = (links) => {
+        browser.runtime.sendMessage({
+            command: "links",
+            links: links,
+        });
+    };
     browser.runtime.onMessage.addListener((message) => {
         const { command, pattern, useRegex, caseSensitive } = message;
+        localStorage.setItem("filter", JSON.stringify({ pattern, useRegex, caseSensitive }));
         if (command === "getLinks") {
-            browser.runtime.sendMessage({
-                command: "links",
-                links: getLinks(
-                    useRegex ? new RegExp(pattern) : pattern,
-                    caseSensitive
-                ),
-            });
+            sendLinks(getLinks(useRegex ? new RegExp(pattern) : pattern, caseSensitive));
         }
     });
-
-    const links = getLinks("");
-    browser.runtime.sendMessage({ command: "links", links: links });
+    const storedFilter = localStorage.getItem("filter");
+    if (storedFilter) {
+        const filter = JSON.parse(storedFilter);
+        browser.runtime.sendMessage({ command: "filter", ...filter });
+    }
+    else {
+        browser.runtime.sendMessage({
+            command: "filter",
+            pattern: "",
+            useRegex: false,
+            caseSensitive: false,
+        });
+    }
 })();
