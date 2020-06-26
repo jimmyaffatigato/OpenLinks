33deab3c8b986b500b568bba7ca22de8bc43e1a9
jimmyaffatigato <jimmyaffatigato@gmail.com>
2020-06-26 15:42:52 -0400

    First phase of redesign

--- src/extensionScript.ts
+++ src/extensionScript.ts
@@ -1,7 +1,11 @@
 const goButton = document.getElementById("goButton") as HTMLButtonElement;
-const patternInput = document.getElementById("patternInput") as HTMLInputElement;
+const patternInput = document.getElementById(
+    "patternInput"
+) as HTMLInputElement;
 const useRegex = document.getElementById("useRegex") as HTMLInputElement;
-const caseSensitive = document.getElementById("caseSensitive") as HTMLInputElement;
+const caseSensitive = document.getElementById(
+    "caseSensitive"
+) as HTMLInputElement;
 
 let links = [];
 
@@ -33,17 +37,27 @@ const listenForClicks = () => {
     useRegex.addEventListener("change", pollLinks);
     caseSensitive.addEventListener("change", pollLinks);
 };
-browser.tabs.executeScript({ file: "scripts/openLinks.js" }).then(listenForClicks);
+browser.tabs
+    .executeScript({ file: "scripts/openLinks.js" })
+    .then(listenForClicks);
 
 browser.runtime.onMessage.addListener((message) => {
     if (message.command === "links") {
+        let color = Math.floor((message.links.length / 50) * 255);
+        if (color > 255) {
+            color = 255;
+        }
         links = message.links;
-        document.getElementById("linkCount").innerHTML = `Found ${message.links.length} link${
+        document.getElementById(
+            "linkCount"
+        ).innerHTML = `Found <span style="${`color:#${color.toString(
+            16
+        )}0000`}">${message.links.length}</span> link${
             message.links.length !== 1 ? "s" : ""
         }.`;
         document.getElementById("links").innerHTML = message.links
-            .map((link) => {
-                return `<a class="link" href="${link}">${link}</a>`;
+            .map((link: string) => {
+                return `<a class="link" title="${link}" href="${link}">${link}</a>`;
             })
             .join("\n");
     }
