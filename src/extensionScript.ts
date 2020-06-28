6b9bdd9783b11db52a1784e039c4b6f8f3953483
jimmyaffatigato <jimmyaffatigato@gmail.com>
2020-06-27 20:15:28 -0400

    Filter now stored in browser.storage Content scripts run once

--- src/extensionScript.ts
+++ src/extensionScript.ts
@@ -1,15 +1,30 @@
+interface FilterSettings {
+    pattern: string;
+    useRegex: boolean;
+    ignoreCase: boolean;
+    removeDuplicates: boolean;
+}
+
+browser.browserAction.setBadgeBackgroundColor({ color: "#ffffff" });
+
 // Elements
-const goButton = document.getElementById("goButton") as HTMLButtonElement;
+const openLinksButton = document.getElementById("goButton") as HTMLButtonElement;
 const patternInput = document.getElementById("patternInput") as HTMLInputElement;
-const useRegex = document.getElementById("useRegex") as HTMLInputElement;
-const ignoreCase = document.getElementById("ignoreCase") as HTMLInputElement;
-const removeDuplicates = document.getElementById("removeDuplicates") as HTMLInputElement;
+const useRegexCheckbox = document.getElementById("useRegex") as HTMLInputElement;
+const ignoreCaseCheckbox = document.getElementById("ignoreCase") as HTMLInputElement;
+const removeDuplicatesCheckbox = document.getElementById("removeDuplicates") as HTMLInputElement;
+const test = document.getElementById("test") as HTMLSpanElement;
 const version = document.getElementById("version") as HTMLAnchorElement;
 
 // Manifest
 const manifest = browser.runtime.getManifest();
 version.textContent = `v${manifest.version}`;
 version.href = manifest.homepage_url;
+browser.storage.local.get({ runs: 0 }).then((result) => {
+    const oldRuns = result.runs;
+    test.textContent = oldRuns;
+    browser.storage.local.set({ runs: oldRuns + 1 });
+});
 
 let links = [];
 
@@ -19,41 +34,65 @@ const openLinks = (links: string[]) => {
     });
 };
 
-const pollLinks = (): void => {
-    const pattern = patternInput.value;
+const saveFilter = () => {
     browser.tabs.query({ active: true, currentWindow: true }).then((tabs: browser.tabs.Tab[]) => {
-        browser.tabs.sendMessage(tabs[0].id, {
-            command: "getLinks",
-            filterSettings: {
-                pattern: pattern,
-                useRegex: useRegex.checked,
-                ignoreCase: ignoreCase.checked,
-                removeDuplicates: removeDuplicates.checked,
+        const tab = tabs[0];
+        browser.storage.local.set({
+            [String(tab.id)]: {
+                pattern: patternInput.value,
+                useRegex: useRegexCheckbox.checked,
+                ignoreCase: ignoreCaseCheckbox.checked,
+                removeDuplicates: removeDuplicatesCheckbox.checked,
             },
         });
     });
 };
 
-const listenForClicks = () => {
-    goButton.addEventListener("click", (): void => {
-        openLinks(links);
+const pollLinks = (): void => {
+    browser.tabs.query({ active: true, currentWindow: true }).then((tabs: browser.tabs.Tab[]) => {
+        const tab = tabs[0];
+        browser.tabs.sendMessage(tab.id, {
+            command: "getLinks",
+            filterSettings: {
+                pattern: patternInput.value,
+                useRegex: useRegexCheckbox.checked,
+                ignoreCase: ignoreCaseCheckbox.checked,
+                removeDuplicates: removeDuplicatesCheckbox.checked,
+            },
+        });
     });
-
-    patternInput.addEventListener("input", pollLinks);
-    useRegex.addEventListener("change", pollLinks);
-    ignoreCase.addEventListener("change", pollLinks);
-    removeDuplicates.addEventListener("change", pollLinks);
 };
-browser.tabs.executeScript({ file: "scripts/openLinks.js" }).then(listenForClicks);
+
+browser.tabs.executeScript({ file: "scripts/openLinks.js" }).then(() => {
+    browser.tabs.query({ active: true, currentWindow: true }).then((tabs: browser.tabs.Tab[]) => {
+        const tab = tabs[0];
+        browser.storage.local
+            .get({
+                [String(tab.id)]: { pattern: "", useRegex: false, ignoreCase: false, removeDuplicates: false },
+            })
+            .then((result) => {
+                console.log(result);
+                const { pattern, useRegex, ignoreCase, removeDuplicates } = result[String(tab.id)]
+                    ? result[String(tab.id)]
+                    : { pattern: "", useRegex: false, ignoreCase: false, removeDuplicates: false };
+                patternInput.value = pattern;
+                useRegexCheckbox.checked = useRegex;
+                ignoreCaseCheckbox.checked = ignoreCase;
+                removeDuplicatesCheckbox.checked = removeDuplicates;
+                pollLinks();
+            });
+    });
+});
 
 browser.runtime.onMessage.addListener((message) => {
     if (message.command === "links") {
-        let color = Math.floor((message.links.length / 50) * 255);
-        if (color > 255) {
-            color = 255;
+        let r = Math.floor((message.links.length / 50) * 255);
+        if (r > 255) {
+            r = 255;
         }
+        const color = `#${r.toString(16)}0000`;
         links = message.links;
-        document.getElementById("linkCount").innerHTML = `Found <span style="${`color:#${color.toString(16)}0000`}">${
+        document.getElementById("linkCount").innerHTML = `Found <span style="color:${color}">${
             message.links.length
         }</span> link${message.links.length !== 1 ? "s" : ""}.`;
         document.getElementById("links").innerHTML = message.links
@@ -61,12 +100,27 @@ browser.runtime.onMessage.addListener((message) => {
                 return `<a class="link" title="${link}" href="${link}">${link}</a>`;
             })
             .join("\n");
+        browser.browserAction.setBadgeText({ text: String(links.length) });
+        browser.browserAction.setBadgeTextColor({ color: color });
     }
-    if (message.command === "filter") {
-        patternInput.value = message.pattern;
-        useRegex.checked = message.useRegex;
-        ignoreCase.checked = message.caseSensitive;
-        removeDuplicates.checked = message.removeDuplicates;
-        pollLinks();
-    }
+});
+
+openLinksButton.addEventListener("click", () => {
+    openLinks(links);
+});
+patternInput.addEventListener("input", () => {
+    saveFilter();
+    pollLinks();
+});
+useRegexCheckbox.addEventListener("change", () => {
+    saveFilter();
+    pollLinks();
+});
+ignoreCaseCheckbox.addEventListener("change", () => {
+    saveFilter();
+    pollLinks();
+});
+removeDuplicatesCheckbox.addEventListener("change", () => {
+    saveFilter();
+    pollLinks();
 });
--- src/openLinks.ts
+++ src/openLinks.ts
@@ -6,55 +6,45 @@ interface FilterSettings {
 }
 
 (() => {
-    const getLinks = (filterSettings: FilterSettings) => {
-        const { pattern, useRegex, ignoreCase, removeDuplicates } = filterSettings;
-        const linkElements = document.getElementsByTagName("a");
-        let links = [] as string[];
-        for (let i = 0; i < linkElements.length; i++) {
-            links.push(linkElements[i].href);
-        }
-        if (removeDuplicates) {
-            const set = new Set(links);
-            links = [...set];
-        }
-        const matchingLinks = links.filter((link) => {
-            if (useRegex) {
-                return link.match(RegExp(pattern, `${ignoreCase ? "i" : ""}`));
-            } else {
-                if (ignoreCase) {
-                    return link.toLowerCase().includes(pattern.toLowerCase());
-                } else {
-                    return link.includes(pattern);
-                }
+    if (!window["hasRun"]) {
+        window["hasRun"] = true;
+        const getLinks = (filterSettings: FilterSettings) => {
+            const { pattern, useRegex, ignoreCase, removeDuplicates } = filterSettings;
+            const linkElements = document.getElementsByTagName("a");
+            let links = [] as string[];
+            for (let i = 0; i < linkElements.length; i++) {
+                links.push(linkElements[i].href);
             }
-        });
-        return matchingLinks;
-    };
-    const sendLinks = (links: string[]) => {
-        browser.runtime.sendMessage({
-            command: "links",
-            links: links,
-        });
-    };
+            if (removeDuplicates) {
+                const set = new Set(links);
+                links = [...set];
+            }
+            const matchingLinks = links.filter((link) => {
+                if (useRegex) {
+                    return link.match(RegExp(pattern, `${ignoreCase ? "i" : ""}`));
+                } else {
+                    if (ignoreCase) {
+                        return link.toLowerCase().includes(pattern.toLowerCase());
+                    } else {
+                        return link.includes(pattern);
+                    }
+                }
+            });
+            return matchingLinks;
+        };
+        const sendLinks = (links: string[]) => {
+            browser.runtime.sendMessage({
+                command: "links",
+                links: links,
+            });
+        };
 
-    browser.runtime.onMessage.addListener((message: { command: string; filterSettings: FilterSettings }) => {
-        if (message.command == "getLinks") {
-            const { filterSettings } = message;
-            localStorage.setItem("filter", JSON.stringify(filterSettings));
-            sendLinks(getLinks(filterSettings));
-        }
-    });
-
-    const storedFilter = localStorage.getItem("filter");
-    if (storedFilter) {
-        const filter = JSON.parse(storedFilter) as FilterSettings;
-        browser.runtime.sendMessage({ command: "filter", ...filter });
-    } else {
-        browser.runtime.sendMessage({
-            command: "filter",
-            pattern: "",
-            useRegex: false,
-            caseSensitive: false,
+        browser.runtime.onMessage.addListener((message: { command: string; filterSettings: FilterSettings }) => {
+            if (message.command == "getLinks") {
+                const { filterSettings } = message;
+                console.log(filterSettings);
+                sendLinks(getLinks(filterSettings));
+            }
         });
     }
 })();
